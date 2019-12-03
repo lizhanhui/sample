120ea58e24abd8ce8fa1457177e19da7e6a0848e
Li Zhanhui <lizhanhui@apache.org>
2019-12-03 12:45:58 +0800

    Initial commit

---
+++ library.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+#include "library.h"
+
+void hello() { std::cout << "Hello, World!" << std::endl; }
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef DUMMY_LIBRARY_H
+#define DUMMY_LIBRARY_H
+
+void hello();
+
+#endif  // DUMMY_LIBRARY_H
