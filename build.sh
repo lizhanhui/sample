227efb68e5189ebbe16ac2de57086721dc6062b6
Li Zhanhui <lizhanhui@apache.org>
2019-12-03 14:27:34 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+work_dir=$(dirname "$0")
+echo "Current working directory: $work_dir"
+cd $work_dir
+work_dir=$(pwd)
+if [[ -d _build ]]; then
+    rm -fr _build
+fi
+mkdir _build
+cd _build
+echo `pwd`
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$work_dir ..
+make
+make install
+
